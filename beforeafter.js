e39821220c701f3ec1052ca1a3ab22423e8f3c6e
TomiServis <pancisintomas@gmail.com>
2026-06-14 22:03:54 +0200

    Create beforeafter.js

---
+++ beforeafter.js
@@ -0,0 +1,12 @@
+const slider = document.getElementById("slider");
+const after = document.querySelector(".after");
+
+if(slider && after){
+
+    slider.addEventListener("input", function(){
+
+        after.style.width = this.value + "%";
+
+    });
+
+}
